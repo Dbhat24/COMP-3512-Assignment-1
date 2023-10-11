23b0c95f197e0fb4d50c6f72c920393d4fb4e2fa
Raj Bhatti <dbhat325@mtroyal.ca>
2023-10-11 10:38:51 -0600

    initial commit

---
+++ single-song-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+    <title>COMP 3512 Assign1</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./style/single-song-page.css"> 
+    </head>
+
+    <body>
+        <article>
+            <header>
+                <h1>Raj Bhatti</h1>
+                <nav class ="navigation">
+                    <a href="">Home</a>
+                    <a href="">Browse</a>
+                    <a href="">Search</a>
+                    <a href="">About us</a>  
+                </nav>    
+            </header> 
+        </article>     
+
+
